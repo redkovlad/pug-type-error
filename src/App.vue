8115114d07754d399cbba90337245d13164486ab
Владислав Редько <vladislav.redko@sholding.ru>
2022-11-27 13:02:24 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PugComponent from './components/PugComponent.vue'
+</script>
+
+<template>
+  <PugComponent></PugComponent>
+</template>
---
+++ src/components/PugComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import '../assets/css/counterStyle.css'
+import { usePiniaStore } from '@/stores/piniaStore';
+
+const store = usePiniaStore();
+
+setInterval(() => store.increment(), 1000);
+</script>
+<template>
+    <div class="counter">{{ store.count }}</div>
+</template>
